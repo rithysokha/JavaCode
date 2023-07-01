b09f73e2b5fc7b2935071205307a7b53600fe445
idkwhoami <124172865+rithysokha@users.noreply.github.com>
2023-07-01 10:13:44 +0700

    1 july morning

--- The Four Pillars of OOP/Homework/src/exercise5/Student.java
+++ The Four Pillars of OOP/Homework/src/exercise5/Student.java
@@ -1,23 +1,50 @@
 package exercise5;
 
-class Sudent {
+import java.util.ArrayList;
+
+public class Student {
     private int studentID;
     private String studentName;
+    private ArrayList<String> classes;
+    private ArrayList<Double> gradees;
 
-    public int getStudentID() {
+    // setter getter for private variables
+    int getStudentID() {
         return studentID;
     }
 
-    public void setStudentID(int studentID) {
+    void setStudentID(int studentID) {
         this.studentID = studentID;
     }
 
-    public String getStudentName() {
+    String getStudentName() {
         return studentName;
     }
 
-    public void setStudentName(String studentName) {
+    void setStudentName(String studentName) {
         this.studentName = studentName;
     }
 
+    public ArrayList<String> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(ArrayList<String> classes) {
+        this.classes = classes;
+    }
+
+    public ArrayList<Double> getGradees() {
+        return gradees;
+    }
+
+    public void setGradees(ArrayList<Double> gradees) {
+        this.gradees = gradees;
+    }
+
+    Student(int studentID, String studentName) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+        this.classes = new ArrayList<String>();
+        this.gradees = new ArrayList<Double>();
+    }
 }
